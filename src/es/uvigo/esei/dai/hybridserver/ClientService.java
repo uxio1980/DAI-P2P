bb699ebc9aeea50d228641f32f7800fe19203a25
Uxío González <uxio24@hotmail.com>
2017-10-04 12:04:42 +0200

    First Commit
    week2

--- src/es/uvigo/esei/dai/hybridserver/ClientService.java
+++ src/es/uvigo/esei/dai/hybridserver/ClientService.java
@@ -1,13 +1,43 @@
 package es.uvigo.esei.dai.hybridserver;
 
-public class ClientService {
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// HTTP request y response
+import es.uvigo.esei.dai.hybridserver.http.HTTPHeaders;
+import es.uvigo.esei.dai.hybridserver.http.HTTPParseException;
+import es.uvigo.esei.dai.hybridserver.http.HTTPRequest;
+import es.uvigo.esei.dai.hybridserver.http.HTTPResponse;
+import es.uvigo.esei.dai.hybridserver.http.HTTPResponseStatus;
 
+public class ClientService implements Runnable {
+
+	private Socket socket;
+
+	public ClientService(Socket socket) {
+		this.socket = socket;
+	}
+
+	@Override
+	public void run() {
+		try {
+			HTTPRequest request = new HTTPRequest(
+					new InputStreamReader(socket.getInputStream()));
+			HTTPResponse response = new HTTPResponse();
+			response.setVersion(HTTPHeaders.HTTP_1_1.getHeader());
+			response.setStatus(HTTPResponseStatus.S200);
+			response.setContent("Hybrid Server");
+
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			out.println(response);
+			//socket.getOutputStream();
+			socket.getOutputStream().flush();
+		} catch (IOException | HTTPParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(">> ClientService " + Thread.currentThread().getName() + " sirvió la página...");
 	}
 
 }
--- src/es/uvigo/esei/dai/hybridserver/HybridServer.java
+++ src/es/uvigo/esei/dai/hybridserver/HybridServer.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Properties;
 
@@ -19,11 +20,11 @@ public class HybridServer {
 	private Thread serverThread;
 	private boolean stop;
 	private Map<String, String> pages;
-	
+
 	public HybridServer() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public HybridServer(Map<String, String> pages) {
 		// TODO Mapa con <UID, contenido(para poost)> Lo hace el test
 		this.pages = pages;
@@ -36,32 +37,45 @@ public class HybridServer {
 	public int getPort() {
 		return SERVICE_PORT;
 	}
-	
+
 	public void start() {
 		this.serverThread = new Thread() {
 			@Override
 			public void run() {
+				System.out.print("Inicializando servidor... ");
+				ArrayList<Socket> arraySockets = new ArrayList<Socket>();
+				Socket socket = null;
 				try (final ServerSocket serverSocket = new ServerSocket(SERVICE_PORT)) {
+					System.out.println("\t[OK]\nEsperando conexiones entrantes...");
+					int idSession = 0;   
 					while (true) {
-						try (Socket socket = serverSocket.accept()) {
+						//try (Socket socket = serverSocket.accept()) {
+						try  {
+							socket = serverSocket.accept();
+							System.out.println("Nueva conexión entrante: "+socket);
+							arraySockets.add(socket);
+							idSession++;
 							if (stop) break;
-							
-							// Crear hilos de servicio para los clientes:
-							HTTPRequest request = new HTTPRequest(
-									new InputStreamReader(socket.getInputStream()));
-							
-							HTTPResponse response = new HTTPResponse();
-							response.setVersion(HTTPHeaders.HTTP_1_1.getHeader());
-							response.setStatus(HTTPResponseStatus.S200);
-							response.setContent("Hybrid Server");
-							//System.out.println("Response Code : " + response);
-							 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-							 out.println(response);
-							//socket.getOutputStream();
-							socket.getOutputStream().flush();
-						} catch (HTTPParseException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+							//new Thread (new ClientService(socket)).start();;
+							try {
+								HTTPRequest request = new HTTPRequest(
+										new InputStreamReader(socket.getInputStream()));
+								HTTPResponse response = new HTTPResponse();
+								response.setVersion(HTTPHeaders.HTTP_1_1.getHeader());
+								response.setStatus(HTTPResponseStatus.S200);
+								response.setContent("Hybrid Server");
+
+								PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+								out.println(response);
+								//socket.getOutputStream();
+								socket.getOutputStream().flush();
+							} catch (IOException | HTTPParseException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						} catch (Exception e) {
+							System.out.println("Error en servidor:\n" + e.getMessage());
+							System.exit(0);
 						}
 					}
 				} catch (IOException e) {
@@ -72,23 +86,24 @@ public class HybridServer {
 
 		this.stop = false;
 		this.serverThread.start();
+		//while (this.serverThread.isAlive());
 	}
-	
+
 	public void stop() {
 		this.stop = true;
-		
+
 		try (Socket socket = new Socket("localhost", SERVICE_PORT)) {
 			// Esta conexiÃ³n se hace, simplemente, para "despertar" el hilo servidor
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		try {
 			this.serverThread.join();
 		} catch (InterruptedException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		this.serverThread = null;
 	}
 }
